8f608804cacf81ffa82095f2472243a3f6dcf3f7
Brian Guterl <brianguterl99@gmail.com>
2024-01-09 18:57:19 -0500

    day4part1 finished

--- day4/index.js
+++ day4/index.js
@@ -13,7 +13,47 @@ const input = readFileSync("input.txt", { encoding: "utf-8" })
 function day4part1(cards) {
   const cardPoints = [];
 
-  return cards;
+  //check cards
+  for (const card of cards) {
+    let winningPoints = 0;
+
+    //grab winning numbers
+    const winningNumbers = card
+      .slice(/\:/.exec(card).index + 1, /\|/.exec(card).index)
+      .trim()
+      .split(" ");
+
+    //grab player numbers
+    const playedNumbers = card
+      .slice(/\|/.exec(card).index + 1)
+      .trim()
+      .split(/\s+/g);
+
+    //check which player numbers are winners and get points
+    for (let num of playedNumbers) {
+      if (winningNumbers.includes(num)) {
+        winningPoints === 0 ? (winningPoints = 1) : (winningPoints *= 2);
+      }
+    }
+
+    //save points
+    cardPoints.push(winningPoints);
+
+    // console.log(
+    //   "winning numbers:",
+    //   winningNumbers,
+    //   "played numbers",
+    //   playedNumbers,
+    //   "points",
+    //   winningPoints
+    // );
+  }
+
+  //add up total points
+  return cardPoints.reduce((a, b) => a + b);
 }
 
 console.log(day4part1(example));
+console.log(day4part1(input));
+
+function day4part2(cards) {}
