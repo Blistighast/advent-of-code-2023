d5e613556e17ba6250a7c7cbcc390cd76181adc5
Brian Guterl <brianguterl99@gmail.com>
2024-01-12 02:59:17 -0500

    day5 part1 set up

---
+++ day5/index.js
@@ -0,0 +1,23 @@
+const { readFileSync } = require("fs");
+
+const example = readFileSync("example.txt", { encoding: "utf-8" })
+  .replace(/\r/g, "") //remove all \r characters (carriage return) just in case
+  .trim() // remove white space at beginning and end
+  .split("\n"); //split text into array of each line
+
+const input = readFileSync("input.txt", { encoding: "utf-8" })
+  .replace(/\r/g, "") //remove all \r characters (carriage return) just in case
+  .trim() // remove white space at beginning and end
+  .split("\n"); //split text into array of each line
+
+function inputFormatter(input) {
+  const almanacMap = {};
+
+  return almanacMap;
+}
+
+function day5part1(almanac) {
+  return almanac;
+}
+
+console.log(day5part1(example));
